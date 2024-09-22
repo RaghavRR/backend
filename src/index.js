63db4dbc1a895cc69ad1c2d0edd4ca09eee445a0
Raghav Rastogi <raghavrastogi2021@gmail.com>
2024-09-22 11:55:08 +0530

    mongoDB connected

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv"
+import express from "express"
+import connectDB from "./db/index.js";
+const app = express()
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+/*
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)        
+        app.on("error",(error)=>{
+            console.log("Express is not able to talk to database");
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("ERROR: ",error);
+        throw err
+        
+    }
+})()
+
+*/
