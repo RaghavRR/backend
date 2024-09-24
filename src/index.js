e9d99228b00324c5301325ad4f03b0c2b187ad07
Raghav Rastogi <raghavrastogi2021@gmail.com>
2024-09-24 22:38:43 +0530

    add register

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./db/index.js";
-const app = express()
+import {app} from './app.js'
+
 
 dotenv.config({
     path: './env'
